6174e150e65e7b79a4d696215dbb0368f78938d5
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-01-09 19:58:07 -0300

    Atualização da minha branch e tentativa de edição do video

--- Interface1.js
+++ Interface1.js
@@ -1,10 +1,33 @@
+/*
+//interação com ususario, pergunta
 var nome = window.prompt('Qual é o seu nome?');
 window.alert('É um prazer te conhecer, ' + nome + '!');
 var idade = window.prompt('Quantos anos você tem?');
+
+//resposta do codigo.
 window.alert('Que legal, você tem ' + idade + ' anos de idade!');
 window.alert('Que falta de educação a minha, deixe eu me apresentar...');
 window.alert('Olá, eu sou o computador que este usuário usa!');
-window.prompt('Você conhece o usuário deste computador?');
+
+//variavel para o sim e não.
+let per = prompt('Você conhece o usuario desse computador');
+
+//Variavel para dizer o estado de saúde do usuario, extremamente apaixonado.
+//If e else ou else if
+
+//sim e não, variavel para
+
+if (pergunta = 'sim') {
+    document.write("CUIDADO ele esta TOTALMENTE APAIXONADO POR VOCÊ");
+}
+//Caso esteja a baixo entre 24 e 29.9 peso recomendado
+else if(pergunta = 'nao') {
+    document.write("Você esa segura : )")
+}
+
+window.alert("" + per + " bom isso e apenas o começo não tem problema ");
+//Como arredondar um número em JavaScript?
+
 window.alert('Que legal que você conhece ele!');
 window.prompt('Sabia que ele me fala muito sobre você, ' + nome + '?');
 window.alert('Pois é, ele fala bastante de você!');
@@ -12,9 +35,12 @@ window.alert('Ele gosta muito de você!');
 window.prompt('Posso te mostrar o que ele fala de você?');
 window.alert('Então, irei te mostra o que fala de você para mim, ao som desta musica que vai tocar agora!');
 
+//futuras atualizaçôes
+/*
 $('button').on(onmouseover, function() {
     $(this).animate({
         'top': Math.random() * document.getElementById('content').clientHeight + "px",
         'left': Math.random() * document.getElementById('content').clientWidth + "px"
     },  "medium");
 })
+*/
